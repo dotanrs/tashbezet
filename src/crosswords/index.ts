1821bf4335268ce616eaadd82df0c1aed83867af
Dotan <dotanrs@gmail.com>
2025-03-08 18:56:14 +0200

    Reorder

--- src/crosswords/index.ts
+++ src/crosswords/index.ts
@@ -2,9 +2,9 @@ import { puzzle1 } from './puzzle1';
 import { puzzle2 } from './puzzle2';
 import { puzzle3 } from './puzzle3';
 export const puzzles = {
-  puzzle1,
-  puzzle2,
   puzzle3,
+  puzzle2,
+  puzzle1,
 };
 
 export type PuzzleId = keyof typeof puzzles; 
