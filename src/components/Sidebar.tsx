5289313cf007852efc1e59331b824285ecb2f99c
Dotan <dotanrs@gmail.com>
2025-03-15 17:50:36 +0200

    Move buttons down

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './Button';
 
 interface SidebarProps {
   onMarkPuzzle: () => void;
@@ -15,66 +16,39 @@ const Sidebar: React.FC<SidebarProps> = ({
   onReset,
   hasUntestedCells,
   hasAvailableHints,
-  message
+  message,
 }) => {
   return (
-    <div id="sidebar">
-      <div>
+      <>
         {/* Status message */}
-        {message && (
+        {/* {message && (
           <div className="mb-6 p-2 rounded bg-green-100 text-green-800" style={{ direction: 'rtl' }}>
             {message}
           </div>
-        )}
+        )} */}
 
         {/* Buttons section */}
-        <div className="flex flex-col gap-2 text-[13px] w-[90px]">
-          <button
-            onClick={onMarkPuzzle}
-            disabled={!hasUntestedCells}
-            className={`px-3 py-2 rounded border-[1px] border-gray-800 ${
-              hasUntestedCells 
-                ? 'hover:bg-[#98e0db] bg-[#dbfcfa] text-black' 
-                : 'bg-gray-300 text-gray-800 cursor-not-allowed'
-            }`}
-          >
-            <div className="flex flex-row justify-between">
-              🖋️
-              <div>
-                בדיקה
-              </div>
-            </div>
-          </button>
-          <button
-            onClick={onHint}
-            disabled={!hasAvailableHints}
-            className={`px-3 py-2 rounded border-[1px] border-gray-800 ${
-              hasAvailableHints 
-                ? 'hover:bg-yellow-100 bg-[#dbfcfa] text-black' 
-                : 'bg-gray-300 text-gray-800 cursor-not-allowed'
-            }`}
-          >
-            <div className="flex flex-row justify-between">
-              🤔
-              <div>
-                רמז
-              </div>
-            </div>
-          </button>
-          <button
-            onClick={onReset}
-            className="px-3 py-2 text-black border-[1px] border-gray-800 text-black rounded bg-[#dbfcfa] hover:bg-red-200"
-          >
-            <div className="flex flex-row justify-between">
-              🧹
-              <div>
-                איפוס
-              </div>
-            </div>
-          </button>
-        </div>
-      </div>
-    </div>
+      <Button
+        onClick={onMarkPuzzle}
+        disabled={!hasUntestedCells}
+        icon="🖋️"
+        text="בדיקה"
+      />
+      <Button
+        onClick={onHint}
+        disabled={!hasAvailableHints}
+        icon="🤔"
+        text="רמז"
+        hoverColor="hover:bg-yellow-100"
+      />
+      <Button
+        onClick={onReset}
+        icon="🧹"
+        text="איפוס"
+        hoverColor="hover:bg-red-200"
+        disabledStyle={false}
+      />
+    </>
   );
 };
 
