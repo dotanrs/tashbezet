5a213f1b83ce83f2b5ddd959c8d5db1380bb95db
Dotan <dotanrs@gmail.com>
2025-06-13 15:20:05 +0300

    Restore ideal max height

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -648,7 +648,7 @@ const Puzzle: React.FC<PuzzlesProps> = ({ currentConfig, currentPuzzleId, setCur
 
   const useSmallScreenAdjustments = windowSize.width < 388;
   const useVerySmallScreenAdjustments = windowSize.width < 325;
-  const sideBarSpacing = useSmallScreenAdjustments ? 'px-0 gap-1' : 'px-2 gap-2'
+  const sideBarSpacing = useSmallScreenAdjustments ? 'px-1 gap-1' : 'px-2 gap-2'
   // Will return true if the game started, but will revert to false if no user actions were made
   const gameAlreadyStarted = secondsElapsed > 0;
 
@@ -656,7 +656,8 @@ const Puzzle: React.FC<PuzzlesProps> = ({ currentConfig, currentPuzzleId, setCur
   <div id="whole-crossword" className={`sm:w-full w-[100%] sm:pt-10 pt-[35px] max-w-[500px] ${hidden && 'hidden'}`}>
     <div id="main-content" style={isMobile ? { minHeight: `calc(var(--app-height) - ${bottomPadding}px - 15px)` } : undefined}>
       <div id="crossword-and-buttons" className={`flex space-x-5 flex-row justify-between items-start mx-auto mt-0 mb-3`}
-        style={{maxWidth: isMobile ? 'calc(var(--app-height) - 370px)' : 'calc(100vh-190px)'}}>
+        style={{maxWidth: isMobile ? 'calc(var(--app-height) - 335px)' : 'calc(100vh-190px)'}}>
+
         <CrosswordGrid
           userGrid={userGrid}
           cellStatus={cellStatus}
